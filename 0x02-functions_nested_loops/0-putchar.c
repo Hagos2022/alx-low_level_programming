99d6ec9f6c93d0541ef69ecc7da2cc96567908d6
Hagos2022 <hagos69@gmail.com>
2022-10-18 13:34:32 -0700

    0x02. C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char *sh = "_putchar\n";
+while (*sh)
+{
+_putchar(*sh);
+sh++;
+}
+return (0);
+}
